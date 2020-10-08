a7186637eb584a637110c5e28487c99781008738
Joey Ortiz <=>
2020-10-08 00:05:04 -0500

    Updates

---
+++ precache-manifest.40839172bc2e5109e7c13d992f472396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d3b7d48e59add22cc042eda1e56c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ab59ecfc5555aca374",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e6e47f7b4d19cbb2b9c",
+    "url": "/static/js/2.2c22cb88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c22cb88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ab59ecfc5555aca374",
+    "url": "/static/js/main.f82bf959.chunk.js"
+  },
+  {
+    "revision": "1cfac94ba95a2fcd5fe3",
+    "url": "/static/js/runtime-main.7de99557.js"
+  }
+]);
